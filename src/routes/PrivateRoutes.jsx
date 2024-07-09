1b80c980691e11e260f43976a4933e5b98d3032e
Roy12547 <163366461+Roy12547@users.noreply.github.com>
2024-07-09 20:20:11 +0800

    Delete src/routes directory

--- src/routes/PrivateRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import React from 'react';
-
-const PrivateRoutes = () => {
-	let auth = { token: true };
-	return auth.token ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoutes;
